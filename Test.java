bd71afdbd4c74cf6e5fe13974be8b91a382c36f2
zhang <zhang@qq.com>
2020-08-28 15:54:07 +0800

    test

--- Test.java
+++ Test.java
@@ -1,17 +1,10 @@
+import java.util.UUID;
 
 public class Test {
 	
 	public static void main(String[] args) {
 		
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
+		UUID.randomUUID().timestamp();
 	}
 
 }
